56adeb0fc3d26ea25ead581b3c1502959824d948
jpb <jpb.06@outlook.com>
2018-03-27 02:38:14 +0200

    Util to promisify functions

---
+++ sources/businesslogic/util/promisify.rebinder.ts
@@ -0,0 +1,8 @@
+﻿declare module 'util' {
+    export function promisify<T>(
+        func: (
+            data: any,
+            cb: (err: NodeJS.ErrnoException, data?: T) => void,
+        ) => void
+    ): (...input: any[]) => Promise<T>;
+}
