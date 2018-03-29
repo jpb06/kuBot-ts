6496417f76fb0e2d2a10a0b18d6fce6f79bf72ab
jpb <jpb.06@outlook.com>
2018-03-29 22:29:40 +0200

    Moving types validation function to an abstract class

--- sources/types/businesslogic/business.types.validation.ts
+++ sources/types/businesslogic/business.types.validation.ts
@@ -1,25 +1,28 @@
 ﻿import * as BusinessTypes from './business.types';
-import * as PersistedTypesValidation from './../dbase/persisted.types.validation';
+import { PersistedTypesValidation } from './../dbase/persisted.types.validation';
 
-export function isScannedFaction(obj: any): obj is BusinessTypes.ScannedFaction {
-    if (typeof obj.name === "string" &&
-        typeof obj.playersCount === "number") {
-        return true;
-    }
+export abstract class BusinessTypesValidation {
 
-    return false;
-}
+    public static IsScannedFaction(obj: any): obj is BusinessTypes.ScannedFaction {
+        if (typeof obj.name === "string" &&
+            typeof obj.playersCount === "number") {
+            return true;
+        }
 
-export function isScannedRegion(obj: any): obj is BusinessTypes.ScannedRegion {
-    if (typeof obj.name !== "string" ||
-        typeof obj.playersCount !== "number" ||
-        !Array.isArray(obj.watchedPlayers)) {
         return false;
     }
 
-    for (let i = 0; i < obj.watchedPlayers.length; i++) {
-        if (!PersistedTypesValidation.isWatchedPlayer(obj.watchedPlayers[i])) return false;
-    }
+    public static IsScannedRegion(obj: any): obj is BusinessTypes.ScannedRegion {
+        if (typeof obj.name !== "string" ||
+            typeof obj.playersCount !== "number" ||
+            !Array.isArray(obj.watchedPlayers)) {
+            return false;
+        }
 
-    return true;
+        for (let i = 0; i < obj.watchedPlayers.length; i++) {
+            if (!PersistedTypesValidation.IsWatchedPlayer(obj.watchedPlayers[i])) return false;
+        }
+
+        return true;
+    }
 }
--- sources/types/dbase/persisted.types.validation.ts
+++ sources/types/dbase/persisted.types.validation.ts
@@ -1,117 +1,120 @@
 ﻿import * as PersistedTypes from './persisted.types';
 
-/* ---------------------------------------------------------------------------------------------------------------
-   Activity Cache
-   ---------------------------------------------------------------------------------------------------------------*/
+export abstract class PersistedTypesValidation {
 
-export function isActivityCacheItem(obj: any): obj is PersistedTypes.ActivityCacheItem {
-    return (
-        typeof obj.name === "string" &&
-        typeof obj.playersCount === "number"
-    );
-}
+    /* ---------------------------------------------------------------------------------------------------------------
+       Activity Cache
+       ---------------------------------------------------------------------------------------------------------------*/
 
-export function isGuildActivityCache(obj: any): obj is PersistedTypes.GuildActivityCache {
-    if (typeof obj.GuildId !== "string" || !Array.isArray(obj.Cache)) {
-        return false;
+    public static IsActivityCacheItem(obj: any): obj is PersistedTypes.ActivityCacheItem {
+        return (
+            typeof obj.name === "string" &&
+            typeof obj.playersCount === "number"
+        );
     }
 
-    for (let i = 0; i < obj.Cache.length; i++) {
-        if (!isActivityCacheItem(obj.Cache[i])) return false;
+    public static IsGuildActivityCache(obj: any): obj is PersistedTypes.GuildActivityCache {
+        if (typeof obj.GuildId !== "string" || !Array.isArray(obj.Cache)) {
+            return false;
+        }
+
+        for (let i = 0; i < obj.Cache.length; i++) {
+            if (!this.IsActivityCacheItem(obj.Cache[i])) return false;
+        }
+
+        return true;
     }
 
-    return true;
-}
+    /* ---------------------------------------------------------------------------------------------------------------
+       Guild Configuration
+       ---------------------------------------------------------------------------------------------------------------*/
 
-/* ---------------------------------------------------------------------------------------------------------------
-   Guild Configuration
-   ---------------------------------------------------------------------------------------------------------------*/
-
-export function isGuildConfiguration(obj: any): obj is PersistedTypes.GuildConfiguration {
-    return (
-        typeof obj.guildId === "string" &&
-        typeof obj.messagesImage === "string" &&
-        typeof obj.messagesFooterName === "string" &&
-        typeof obj.scanMainRegionName === "string" &&
-        typeof obj.mainChannel === "string" &&
-        typeof obj.adminChannel === "string" &&
-        typeof obj.emergencyChannel === "string" &&
-        typeof obj.acknowledged === "string"
-    );
-}
-
-/* ---------------------------------------------------------------------------------------------------------------
-   Last Fetch
-   ---------------------------------------------------------------------------------------------------------------*/
-
-export function isLastFetch(obj: any): obj is PersistedTypes.LastFetch {
-    return (
-        typeof obj.target === "string" &&
-        typeof obj.date === "string"
-    );
-}
-
-/* ---------------------------------------------------------------------------------------------------------------
-   Online Player
-   ---------------------------------------------------------------------------------------------------------------*/
-
-export function isOnlinePlayer(obj: any): obj is PersistedTypes.OnlinePlayer {
-    return (
-        typeof obj.Time === "string" &&
-        typeof obj.Name === "string" &&
-        typeof obj.System === "string" &&
-        typeof obj.Region === "string" &&
-        typeof obj.Ping === "number"
-    );
-}
-
-/* ---------------------------------------------------------------------------------------------------------------
-   Watched Faction
-   ---------------------------------------------------------------------------------------------------------------*/
-
-export function isWatchedFaction(obj: any): obj is PersistedTypes.WatchedFaction {
-    if (typeof obj.guildId !== "string" ||
-        typeof obj.name !== "string" ||
-        typeof obj.alwaysDisplay !== "boolean" ||
-        !Array.isArray(obj.tags)) {
-        return false;
+    public static IsGuildConfiguration(obj: any): obj is PersistedTypes.GuildConfiguration {
+        return (
+            typeof obj.guildId === "string" &&
+            typeof obj.messagesImage === "string" &&
+            typeof obj.messagesFooterName === "string" &&
+            typeof obj.scanMainRegionName === "string" &&
+            typeof obj.mainChannel === "string" &&
+            typeof obj.adminChannel === "string" &&
+            typeof obj.emergencyChannel === "string" &&
+            typeof obj.acknowledged === "string"
+        );
     }
 
-    for (let i = 0; i < obj.tags.length; i++) {
-        if (typeof obj.tags[i] !== "string") return false;
+    /* ---------------------------------------------------------------------------------------------------------------
+       Last Fetch
+       ---------------------------------------------------------------------------------------------------------------*/
+
+    public static IsLastFetch(obj: any): obj is PersistedTypes.LastFetch {
+        return (
+            typeof obj.target === "string" &&
+            typeof obj.date === "string"
+        );
     }
 
-    return true;
-}
+    /* ---------------------------------------------------------------------------------------------------------------
+       Online Player
+       ---------------------------------------------------------------------------------------------------------------*/
 
-/* ---------------------------------------------------------------------------------------------------------------
-   Watched Player
-   ---------------------------------------------------------------------------------------------------------------*/
-
-export function isWatchedPlayer(obj: any): obj is PersistedTypes.WatchedPlayer {
-    if (typeof obj.guildId !== "string" ||
-        typeof obj.name !== "string" ||
-        typeof obj.comment !== "string")
-        return false;
-
-    return true;
-}
-
-/* ---------------------------------------------------------------------------------------------------------------
-   Watched Region
-   ---------------------------------------------------------------------------------------------------------------*/
-
-export function isWatchedRegion(obj: any): obj is PersistedTypes.WatchedRegion {
-    if (typeof obj.guildId !== "string" ||
-        typeof obj.name !== "string" ||
-        typeof obj.alwaysDisplay !== "boolean" ||
-        !Array.isArray(obj.systems)) {
-        return false;
+    public static IsOnlinePlayer(obj: any): obj is PersistedTypes.OnlinePlayer {
+        return (
+            typeof obj.Time === "string" &&
+            typeof obj.Name === "string" &&
+            typeof obj.System === "string" &&
+            typeof obj.Region === "string" &&
+            typeof obj.Ping === "number"
+        );
     }
 
-    for (let i = 0; i < obj.systems.length; i++) {
-        if (typeof obj.systems[i] !== "string") return false;
+    /* ---------------------------------------------------------------------------------------------------------------
+       Watched Faction
+       ---------------------------------------------------------------------------------------------------------------*/
+
+    public static IsWatchedFaction(obj: any): obj is PersistedTypes.WatchedFaction {
+        if (typeof obj.guildId !== "string" ||
+            typeof obj.name !== "string" ||
+            typeof obj.alwaysDisplay !== "boolean" ||
+            !Array.isArray(obj.tags)) {
+            return false;
+        }
+
+        for (let i = 0; i < obj.tags.length; i++) {
+            if (typeof obj.tags[i] !== "string") return false;
+        }
+
+        return true;
     }
 
-    return true;
+    /* ---------------------------------------------------------------------------------------------------------------
+       Watched Player
+       ---------------------------------------------------------------------------------------------------------------*/
+
+    public static IsWatchedPlayer(obj: any): obj is PersistedTypes.WatchedPlayer {
+        if (typeof obj.guildId !== "string" ||
+            typeof obj.name !== "string" ||
+            typeof obj.comment !== "string")
+            return false;
+
+        return true;
+    }
+
+    /* ---------------------------------------------------------------------------------------------------------------
+       Watched Region
+       ---------------------------------------------------------------------------------------------------------------*/
+
+    public static IsWatchedRegion(obj: any): obj is PersistedTypes.WatchedRegion {
+        if (typeof obj.guildId !== "string" ||
+            typeof obj.name !== "string" ||
+            typeof obj.alwaysDisplay !== "boolean" ||
+            !Array.isArray(obj.systems)) {
+            return false;
+        }
+
+        for (let i = 0; i < obj.systems.length; i++) {
+            if (typeof obj.systems[i] !== "string") return false;
+        }
+
+        return true;
+    }
 }
