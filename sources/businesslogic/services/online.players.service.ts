e0dd5647fb47ff1781250825dde1e753b42c0185
jpb <jpb.06@outlook.com>
2018-03-29 02:08:26 +0200

    Transforming backend tasks into service components

---
+++ sources/businesslogic/services/online.players.service.ts
@@ -0,0 +1,44 @@
+﻿import * as moment from 'moment';
+
+import { LastFetchStore } from './../../dal/mongodb/stores/business/last.fetch.store';
+import { LastFetch } from './../../types/dbase/business/last.fetch.type';
+import { OnlinePlayersStore } from './../../dal/mongodb/stores/fetched/online.players.store';
+import { OnlinePlayer } from './../../types/dbase/external/online.player.type';
+
+import { DiscoveryGCRequests } from './../httprequests/discoverygc.requests';
+
+export abstract class OnlinePlayersService {
+
+    public static async GetList(): Promise<OnlinePlayer[]> {
+        let lastFetch = await LastFetchStore.get();
+        let now = moment();
+
+        // request threshold to discogc = 2 minutes 
+        if (!lastFetch || moment(lastFetch.date).add(2, 'm').isBefore(now)) {
+            let cookies = await DiscoveryGCRequests.Auth();
+            let data = await DiscoveryGCRequests.OnlinePlayers(cookies);
+
+            // ??? why escaped quotes
+            let cleaned = '';
+            let online: any;
+            try {
+                cleaned = data.replace(/\\"/g, '"')
+                    .replace(/\\\\\\\\/g, '\\\\')
+                    .replace(/\\"/g, '\"');
+                online = JSON.parse(cleaned);
+
+            } catch (err) {
+                console.log(err);
+                console.log(cleaned);
+            }
+
+            await OnlinePlayersStore.set(online.Players);
+            await LastFetchStore.set(moment().format());
+
+            return online.Players;
+        } else {
+            let onlinePlayers = await OnlinePlayersStore.getAll();
+            return onlinePlayers;
+        }
+    }
+}
