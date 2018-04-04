65d931979f0e7d8d9e2a3b0f450f273e8fab4db1
jpb <jpb.06@outlook.com>
2018-04-04 14:33:17 +0200

    Help command main logic

---
+++ sources/front/commands/help.command.ts
@@ -0,0 +1,35 @@
+﻿import { Message, TextChannel } from 'discord.js';
+import { GuildConfiguration } from './../../types/dbase/persisted.types';
+
+import { EmbedHelper } from './../../businesslogic/util/embed.helper';
+import { ErrorsLogging } from './../../businesslogic/util/errors.logging.helper';
+
+export abstract class HelpCommand {
+    public static async Send(
+        guildSettings: GuildConfiguration,
+        message: Message
+    ) {
+        try {
+            let embedHelper = new EmbedHelper(message.channel as TextChannel, guildSettings);
+
+            embedHelper.sendHelpResponse();
+        } catch (error) {
+            await ErrorsLogging.Save(error);
+            EmbedHelper.Error(message.channel as TextChannel);
+        }
+    }
+
+    public static async SendAdmin(
+        guildSettings: GuildConfiguration,
+        message: Message
+    ) {
+        try {
+            let embedHelper = new EmbedHelper(message.channel as TextChannel, guildSettings);
+
+            embedHelper.sendHelpAdminResponse();
+        } catch (error) {
+            await ErrorsLogging.Save(error);
+            EmbedHelper.Error(message.channel as TextChannel);
+        }
+    }
+}
