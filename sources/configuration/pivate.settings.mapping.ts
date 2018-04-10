0fd99e5d99787d7d442dc133b3be803c3064bb3f
jpb <jpb.06@outlook.com>
2018-04-10 20:31:38 +0200

    Making private settings static

--- sources/configuration/pivate.settings.mapping.ts
+++ sources/configuration/pivate.settings.mapping.ts
@@ -1,13 +1,16 @@
 ﻿import { PrivateConfig } from './private.config.local'
 
-export function initializeSettings(): void {
-    if (!process.env.kuBotConfig) { // case local
-        process.env.kuBotConfig.apiKey = PrivateConfig.apiKey;
-        process.env.kuBotConfig.botId = PrivateConfig.botId;
-        process.env.kuBotConfig.discogcAuthPostData = PrivateConfig.discogcAuthPostData;
-        process.env.kuBotConfig.mongodbUrl = PrivateConfig.mongodbUrl;
-        process.env.kuBotConfig.mongodbBase = PrivateConfig.mongodbBase;
-    } else {
-        // case deploy
+export abstract class PrivateSettings {
+
+    public static async AddToProcess(): Promise<void> {
+        if (!process.env.kuBotConfig) { // case local
+            process.env.kuBotConfig.apiKey = PrivateConfig.apiKey;
+            process.env.kuBotConfig.botId = PrivateConfig.botId;
+            process.env.kuBotConfig.discogcAuthPostData = PrivateConfig.discogcAuthPostData;
+            process.env.kuBotConfig.mongodbUrl = PrivateConfig.mongodbUrl;
+            process.env.kuBotConfig.mongodbBase = PrivateConfig.mongodbBase;
+        } else {
+            // case deploy
+        }
     }
 }
