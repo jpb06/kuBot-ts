19fabb4c938c25c561415c7381f367c3e9ba7483
jpb <jpb.06@outlook.com>
2018-04-12 09:46:58 +0200

    Now saving guild config json files using current date to keep track of changes

--- sources/front/request/guild.config.update.request.ts
+++ sources/front/request/guild.config.update.request.ts
@@ -1,6 +1,7 @@
 ﻿import { Message, TextChannel } from 'discord.js';
 import * as fs from 'fs';
 import { promisify } from 'util';
+import * as moment from 'moment';
 
 import { FilesHelper } from './../../businesslogic/util/files.helper';
 import { GuildConfigurationService } from './../../businesslogic/services/guild.configuration.service';
@@ -18,20 +19,19 @@ export abstract class GuildConfigUpdateRequest {
 
             let fileExtension = attachment.filename.substr(attachment.filename.lastIndexOf('.') + 1);
             if (fileExtension === 'json') {
+                let now = moment().format('DDMMYYYY-HHmmss');
+                let folder = './guildsConfiguration';
                 let channel = <TextChannel>message.channel;
-                let tempFilePath = `./dal/guildsconfiguration/${channel.guild.id}_update.json`;
+                let tempFilePath = `${folder}/${channel.guild.id}_${now}.json`;
 
                 await FilesHelper.Save(attachment.url, tempFilePath);
 
                 const readFile = promisify(fs.readFile);
-                let data = await readFile(tempFilePath, 'utf8');
+                let data = (await readFile(tempFilePath, 'utf8')).toString();
 
-                let updated = await GuildConfigurationService.UpdateFromJson(channel.guild.id, channel.guild.channels.array(), data.join(''));
+                let updated = await GuildConfigurationService.UpdateFromJson(channel.guild.id, channel.guild.channels.array(), data);
                 if (updated) {
-                    let filePath = `./dal/guildsconfiguration/${channel.guild.id}.json`;
-                    await FilesHelper.Remove(filePath);
-                    await FilesHelper.Rename(tempFilePath, filePath);
-
+                    await FilesHelper.Rename(tempFilePath, `${folder}/${channel.guild.id}_${now}_pass.json`);
                     EmbedHelper.SendGuildSettingsInitCompleted();
                 }
             }
