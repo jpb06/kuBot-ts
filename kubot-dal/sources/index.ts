628282944cb61615f2cafb938ff91c4176a6f5f5
jpb <jpb.06@outlook.com>
2018-04-23 16:33:18 +0200

    Moving database logic to its own project for reusability

---
+++ kubot-dal/sources/index.ts
@@ -0,0 +1,4 @@
+﻿import * as Manipulation from './manipulation.export';
+import * as Types from './types.export';
+
+export { Manipulation, Types };
---
+++ kubot-dal/sources/types/persisted.types.ts
@@ -0,0 +1,76 @@
+﻿/* ---------------------------------------------------------------------------------------------------------------
+   Activity Cache
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface ActivityCacheItem {
+    name: string;
+    playersCount: number;
+}
+
+export interface GuildActivityCache {
+    guildId: string;
+    lastMessageId: string;
+    cache: Array<ActivityCacheItem>;
+}
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Guild Configuration
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface GuildConfiguration {
+    guildId: string;
+    messagesImage: string;
+    messagesFooterName: string;
+    scanMainRegionName: string;
+    mainChannelName: string;
+    adminChannelName: string;
+    emergencyChannelName: string;
+    acknowledged: string;
+    activityNoticeMinPlayers: number;
+}
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Last Fetch
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface LastFetch {
+    target: string;
+    date: string;
+}
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Online Player
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface OnlinePlayer {
+    Time: string;
+    Name: string;
+    System: string;
+    Region: string;
+    Ping: number;
+}
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Watched Faction
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface WatchedFaction {
+    guildId: string,
+    name: string,
+    tags: Array<string>,
+    alwaysDisplay: boolean
+}
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Watched Player
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface WatchedPlayer {
+    guildId: string,
+    name: string,
+    comment: string
+}
+
+/* ---------------------------------------------------------------------------------------------------------------
+   Watched Region
+   ---------------------------------------------------------------------------------------------------------------*/
+export interface WatchedRegion {
+    guildId: string,
+    name: string,
+    systems: Array<string>,
+    alwaysDisplay: boolean
+}
