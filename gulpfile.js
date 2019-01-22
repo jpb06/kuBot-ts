679311babe1a528798d16e202c7df7d39a18fadd
jpb <jpb.06@outlook.com>
2019-01-22 21:53:50 +0100

    Deploying in one click

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+﻿/// <binding BeforeBuild='clean, useDevConfig' AfterBuild='generatePackage, zip' Clean='clean' />
+const gulp = require('gulp');
+
+const zipUtil = require('./build-logic/zip.util.js');
+const fsUtil = require('./build-logic/fs.util.js');
+const deployCommands = require('./build-logic/deploy.commands.js');
+
+var pckg = require('./package.json');
+
+gulp.task('useDevConfig', async () => {
+    await fsUtil.useDevConfig();
+});
+
+gulp.task('clean', async () => {
+    await fsUtil.cleanDist();
+});
+
+gulp.task('generatePackage', async () => {
+    await fsUtil.generatePackage();
+});
+
+gulp.task('zip', async () => {
+    await zipUtil.zipDirectory('./dist', `./release/kubot_${pckg.version}.zip`);
+});
+
+gulp.task('deploy', async () => {
+    await fsUtil.cleanDist();
+
+    await fsUtil.useProdConfig();
+
+    await fsUtil.generatePackage();
+
+    await deployCommands.build();
+
+    await zipUtil.zipDirectory('./dist', `./release/kubot_${pckg.version}.zip`);
+
+    await deployCommands.sendFileToDeployServer();
+
+    return deployCommands.deploy();
+
+});
